47f0fb076bd06c70cb562f358a7dcb47b7ae863a
soap <sophiesoapie.xm@gmail.com>
2020-06-11 20:35:22 -0400

    added modid to main class

--- src/main/java/polydungeons/PolyDungeons.java
+++ src/main/java/polydungeons/PolyDungeons.java
@@ -6,6 +6,8 @@ import polydungeons.entity.PolyDungeonsEntities;
 import polydungeons.item.PolyDungeonsItems;
 
 public class PolyDungeons implements ModInitializer {
+    public static final String MODID = "polydungeons";
+
     @Override
     public void onInitialize() {
         PolyDungeonsBlocks.registerAll();
