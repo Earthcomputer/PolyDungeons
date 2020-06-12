4b4ea6a5fbca6e79de34bc488bbbf6d97387e326
soap <sophiesoapie.xm@gmail.com>
2020-06-12 01:27:32 -0400

    Added useful registration methods.

--- src/main/java/polydungeons/block/PolyDungeonsBlocks.java
+++ src/main/java/polydungeons/block/PolyDungeonsBlocks.java
@@ -1,6 +1,24 @@
 package polydungeons.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import polydungeons.PolyDungeons;
+import polydungeons.item.PolyDungeonsItems;
+
 public class PolyDungeonsBlocks {
+    public static HardenedBlock HARDENED_RED_NETHER_BRICKS;
+    public static HardenedBlock HARDENED_CRACKED_RED_NETHER_BRICKS;
+
+    private static <T extends Block> T registerBlock(T block, String id) {
+        Registry.register(Registry.BLOCK, new Identifier(PolyDungeons.MODID, id), block);
+        BlockItem itemBlock = new BlockItem(block, new Item.Settings());
+        PolyDungeonsItems.registerItem(itemBlock, id);
+        return block;
+    }
+
     public static void registerAll() {
 
     }
--- src/main/java/polydungeons/item/PolyDungeonsItems.java
+++ src/main/java/polydungeons/item/PolyDungeonsItems.java
@@ -1,6 +1,15 @@
 package polydungeons.item;
 
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import polydungeons.PolyDungeons;
+
 public class PolyDungeonsItems {
+    public static <T extends Item> T registerItem(T item, String id) {
+        Registry.register(Registry.ITEM, new Identifier(PolyDungeons.MODID, id), item);
+        return item;
+    }
     public static void registerAll() {
 
     }
