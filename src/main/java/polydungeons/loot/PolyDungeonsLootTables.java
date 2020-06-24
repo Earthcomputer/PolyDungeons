d5c2f8d733f7c2e8165a99b1a44744333f93a145
SoapyXM <sophiesoapie.xm@gmail.com>
2020-06-24 00:18:03 -0400

    added a map to the outpost/dungeon that spawns in stronghold chests

--- src/main/java/polydungeons/loot/PolyDungeonsLootTables.java
+++ src/main/java/polydungeons/loot/PolyDungeonsLootTables.java
@@ -2,24 +2,30 @@ package polydungeons.loot;
 
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.Items;
 import net.minecraft.loot.BinomialLootTableRange;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.ExplorationMapLootFunction;
 import net.minecraft.util.Identifier;
+import polydungeons.PolyDungeons;
 import polydungeons.item.PolyDungeonsItems;
+import polydungeons.structures.DungeonData;
 
 public class PolyDungeonsLootTables {
 	// Adding to Minecraft ones
-	private static final Identifier SLIME_ENTITY = new Identifier("minecraft", "entities/slime");
+	private static final Identifier STRONGHOLD = new Identifier("minecraft", "chests/stronghold_corridor");
 
 	public static void registerAll() {
 		LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
-			/*if (SLIME_ENTITY.equals(id)) {
-				FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-						.rolls(BinomialLootTableRange.create(1, 0.025f))
-						.withEntry(ItemEntry.builder(PolyDungeonsItems.SLIMY_ESSENCE).build());
-
-				supplier.pool(poolBuilder);
-			}*/
+			if(STRONGHOLD.equals(id)) {
+				FabricLootPoolBuilder builder =
+						FabricLootPoolBuilder.builder()
+								.rolls(BinomialLootTableRange.create(1, 0.1f))
+								.withEntry(ItemEntry.builder(Items.MAP).build())
+								.withFunction(ExplorationMapLootFunction.create().withDestination(DungeonData.NETHER_DUNGEON).build());
+				supplier.pool(builder);
+			}
 		});
 	}
 }
