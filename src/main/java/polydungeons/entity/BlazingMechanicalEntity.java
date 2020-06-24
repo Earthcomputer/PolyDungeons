b023a93291000373c666c70e5414e8d5f4d52b37
SoapyXM <sophiesoapie.xm@gmail.com>
2020-06-23 23:13:14 -0400

    made the blazing mechanicals a lot tougher than regular blazes

--- src/main/java/polydungeons/entity/BlazingMechanicalEntity.java
+++ src/main/java/polydungeons/entity/BlazingMechanicalEntity.java
@@ -1,11 +1,21 @@
 package polydungeons.entity;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.BlazeEntity;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.World;
 
 public class BlazingMechanicalEntity extends BlazeEntity {
     protected BlazingMechanicalEntity(EntityType<? extends BlazeEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    public static DefaultAttributeContainer.Builder createBlazeAttributes() {
+        DefaultAttributeContainer.Builder builder = BlazeEntity.createBlazeAttributes();
+        builder.add(EntityAttributes.GENERIC_ARMOR, 4.0d);
+        builder.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5d);
+        return builder;
     }
 }
