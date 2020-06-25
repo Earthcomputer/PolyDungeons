fac1339005fce9d1fd8b42415bcfefa628fcd180
SoapyXM <sophiesoapie.xm@gmail.com>
2020-06-25 16:13:21 -0400

    moved the map to the nether fortress loot table

--- src/main/java/polydungeons/loot/PolyDungeonsLootTables.java
+++ src/main/java/polydungeons/loot/PolyDungeonsLootTables.java
@@ -2,23 +2,20 @@ package polydungeons.loot;
 
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.Items;
 import net.minecraft.loot.BinomialLootTableRange;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.ExplorationMapLootFunction;
 import net.minecraft.util.Identifier;
-import polydungeons.PolyDungeons;
-import polydungeons.item.PolyDungeonsItems;
 import polydungeons.structures.DungeonData;
 
 public class PolyDungeonsLootTables {
 	// Adding to Minecraft ones
-	private static final Identifier STRONGHOLD = new Identifier("minecraft", "chests/stronghold_corridor");
+	private static final Identifier FORTRESS = new Identifier("minecraft", "chests/nether_bridge");
 
 	public static void registerAll() {
 		LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
-			if(STRONGHOLD.equals(id)) {
+			if(FORTRESS.equals(id)) {
 				FabricLootPoolBuilder builder =
 						FabricLootPoolBuilder.builder()
 								.rolls(BinomialLootTableRange.create(1, 0.1f))
